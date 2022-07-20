4a268ad55683d3eae4b9633dd5d9399231f80b15
jesus <jesusantonio30122016@gmail.com>
2022-07-20 15:52:48 -0700

    kernel: move std's to beginning, remove uneeded code, remove hidden behaviour? and remove more defines

--- kernel/dev/lapic.c
+++ kernel/dev/lapic.c
@@ -1,25 +1,24 @@
-#include <dev/lapic.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <dev/lapic.h>
 #include <sys/idt.h>
 #include <sys/cpu.h>
 #include <lib/print.h>
 #include <dev/pit.h>
 
-#define LAPIC_EOI 0x0b0 // End of interrupt
-#define LAPIC_SPURIOUS 0x0f0
-#define LAPIC_CMCI 0x2f0 // LVT Corrected machine check interrupt
-#define LAPIC_ICR(X) ((0x300 + ((X) * 0x10))) // Interrupt command register
-#define LAPIC_LVT_TIMER 0x320
-#define LAPIC_TIMER_INITCNT 0x380 // Initial count register
-#define LAPIC_TIMER_CURCNT 0x390 // Current count register
-#define LAPIC_TIMER_DIV 0x3e0
+#define LAPIC_REG_EOI 0x0b0 // End of interrupt
+#define LAPIC_REG_SPURIOUS 0x0f0
+#define LAPIC_REG_CMCI 0x2f0 // LVT Corrected machine check interrupt
+#define LAPIC_REG_ICR(X) ((0x300 + ((X) * 0x10))) // Interrupt command register
+#define LAPIC_REG_LVT_TIMER 0x320
+#define LAPIC_REG_TIMER_INITCNT 0x380 // Initial count register
+#define LAPIC_REG_TIMER_CURCNT 0x390 // Current count register
+#define LAPIC_REG_TIMER_DIV 0x3e0
 #define LAPIC_EOI_ACK 0x00
 #define LAPIC_ICR_SEND_PENDING (1 << 12)
 
 static void *lapic_base;
 static uint8_t spurious_vec;
-static uint8_t cmci_vec;
 static uint8_t timer_vec;
 
 // TODO: make global cpu context?
@@ -34,8 +33,8 @@ static inline void lapic_write(uint32_t reg, uint32_t val) {
 }
 
 static inline void lapic_timer_stop(void) {
-    lapic_write(LAPIC_TIMER_INITCNT, 0);
-    lapic_write(LAPIC_LVT_TIMER, 1 << 16);
+    lapic_write(LAPIC_REG_TIMER_INITCNT, 0);
+    lapic_write(LAPIC_REG_LVT_TIMER, 1 << 16);
 }
 
 static void lapic_handler(void) {
@@ -50,41 +49,36 @@ void lapic_init(void) {
     lapic_base = (void *)((uintptr_t)rdmsr(0x1b) & 0xfffff000);
 
     // Configure spurious IRQ
-    spurious_vec = 0xFF;
+    spurious_vec = 0xff;
     isr[spurious_vec] = &lapic_handler;
-    lapic_write(LAPIC_SPURIOUS, lapic_read(LAPIC_SPURIOUS) | (1 << 8) | spurious_vec);
-
-    // Correction interrupt
-    cmci_vec = idt_allocate_vector();
-    isr[cmci_vec] = &lapic_handler;
-    lapic_write(LAPIC_CMCI, lapic_read(LAPIC_CMCI) | (1 << 8) | cmci_vec);
+    lapic_write(LAPIC_REG_SPURIOUS, lapic_read(LAPIC_REG_SPURIOUS) | (1 << 8) | spurious_vec);
 
     // Timer interrupt
     timer_vec = idt_allocate_vector();
     isr[timer_vec] = &lapic_handler;
-    lapic_write(LAPIC_LVT_TIMER, lapic_read(LAPIC_LVT_TIMER) | (1 << 8) | timer_vec);
+    lapic_write(LAPIC_REG_LVT_TIMER, lapic_read(LAPIC_REG_LVT_TIMER) | (1 << 8) | timer_vec);
 
     print("lapic: Initialized, base=%lx, timer=%i, spurious irq=%i, cmci irq=%i\n", lapic_base, timer_vec, spurious_vec, cmci_vec);
 }
 
 void lapic_eoi(void) {
-    lapic_write(LAPIC_EOI, LAPIC_EOI_ACK);
+    lapic_write(LAPIC_REG_EOI, LAPIC_EOI_ACK);
 }
 
 void lapic_timer_oneshot(uint32_t us, uint32_t vec) {
     lapic_timer_stop();
 
     uint32_t ticks = us * (lapic_freq / 1000000);
-    lapic_write(LAPIC_LVT_TIMER, vec);
-    lapic_write(LAPIC_TIMER_DIV, 0);
-    lapic_write(LAPIC_TIMER_INITCNT, ticks);
+    lapic_write(LAPIC_REG_LVT_TIMER, vec);
+    lapic_write(LAPIC_REG_TIMER_DIV, 0);
+    lapic_write(LAPIC_REG_TIMER_INITCNT, ticks);
 }
 
 void lapic_send_ipi(uint32_t lapic_id, uint32_t vec) {
-    lapic_write(LAPIC_ICR(1), lapic_id << 24);
-    lapic_write(LAPIC_ICR(0), vec);
+    lapic_write(LAPIC_REG_ICR(1), lapic_id << 24);
+    lapic_write(LAPIC_REG_ICR(0), vec);
     // Wait for it to be sent
-    while ((lapic_read(LAPIC_ICR(0)) & LAPIC_ICR_SEND_PENDING) != 0) {
+    while ((lapic_read(LAPIC_REG_ICR(0)) & LAPIC_ICR_SEND_PENDING) != 0) {
         asm volatile ("pause");
     }
 }
@@ -93,18 +87,17 @@ void lapic_timer_calibrate(void) {
     lapic_timer_stop();
 
     // Initialize PIT
-    lapic_write(LAPIC_LVT_TIMER, (1 << 16) | 0xff);
-    lapic_write(LAPIC_TIMER_DIV, 0);
+    lapic_write(LAPIC_REG_LVT_TIMER, (1 << 16) | 0xff);
+    lapic_write(LAPIC_REG_TIMER_DIV, 0);
     pit_set_count(0xffff); // Reset PIT
 
     int init_tick = pit_get_count();
     int samples = 0xfffff;
-    lapic_write(LAPIC_TIMER_INITCNT, (uint32_t)samples);
-    while (lapic_read(LAPIC_TIMER_CURCNT) != 0) {
+    lapic_write(LAPIC_REG_TIMER_INITCNT, (uint32_t)samples);
+    while (lapic_read(LAPIC_REG_TIMER_CURCNT) != 0) {
         asm volatile ("pause");
     }
     int final_tick = pit_get_count();
-
     int total_ticks = init_tick - final_tick;
     lapic_freq = (samples / total_ticks) * PIT_SCALE;
     lapic_timer_stop();
--- kernel/dev/pit.c
+++ kernel/dev/pit.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <stddef.h>
 #include <dev/pit.h>
 #include <lib/print.h>
 #include <sys/cpu.h>
@@ -11,35 +13,20 @@
 #define PIT_CMD_MODE2 4
 
 void pit_init(void) {
-    outb(0x20, 0x11);
-    outb(0xa0, 0x11);
-    outb(0x21, 0x20);
-    outb(0xa1, 0x28);
-    outb(0x21, 0x04);
-    outb(0xa1, 0x02);
-    outb(0x21, 0x01);
-    outb(0xa1, 0x01);
-    outb(0x21, 0x00);
-    outb(0xa1, 0x00);
     pit_set_timer(0x100);
 }
 
 uint16_t pit_get_count(void) {
     uint16_t cnt = 0;
-    asm ("cli");
     outb(PIT_CR, 0x00);
-
     cnt = inb(PIT_CHANNEL(0));
     cnt |= inb(PIT_CHANNEL(0)) << 8;
-    asm ("sti");
     return cnt;
 }
 
 void pit_set_count(uint16_t cnt) {
-    asm ("cli");
     outb(PIT_CHANNEL(0), cnt & 0xff);
     outb(PIT_CHANNEL(0), (cnt >> 8) & 0xff);
-    asm ("sti");
 }
 
 void pit_set_timer(int hz) {
